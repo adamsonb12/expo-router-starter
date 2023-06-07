c3ce64030dad26ea911f549307e0c7ad67e254a8
Brett Adamson <adamsonb12@gmail.com>
2023-06-06 22:27:12 -0600

    eslint and prettier setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import { Slot } from "expo-router";
 
 export default function Root() {
   // TODO
-  // Styled components and Theming
   // ES Lint and Prettier
+  // Styled components and Theming
   // Apollo & graphql mocked out
   // Auth mocked out with routes and redirections
   return (
